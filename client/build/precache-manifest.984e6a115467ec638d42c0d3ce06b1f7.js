2e3ec0a72d7e2799c461bb578712da2ec2fd5796
Armenuhi <avaarm95@gmail.com>
2020-07-24 21:07:56 -0700

    adding build

---
+++ client/build/precache-manifest.984e6a115467ec638d42c0d3ce06b1f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ae05ad162e4fa82a1e1",
+    "url": "/static/css/main.671979c8.chunk.css"
+  },
+  {
+    "revision": "3ae05ad162e4fa82a1e1",
+    "url": "/static/js/main.39a8b058.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7df09a61a2c37d2f3a9",
+    "url": "/static/js/2.5c25870c.chunk.js"
+  },
+  {
+    "revision": "363c24fc1217a11106a74029b97cc859",
+    "url": "/index.html"
+  }
+];
