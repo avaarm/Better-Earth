8b4b3cfa5a9a47b8e66a72c30f2913ea920ed23f
jeffreyadamo <jeffreyadamo@gmail.com>
2020-07-28 19:59:26 -0700

    recommitting

--- client/src/components/NewsCard/index.js
+++ client/src/components/NewsCard/index.js
@@ -1,22 +1,28 @@
 import React from "react";
-import { Row, Col, CardPanel} from 'react-materialize';
+import { Row, Col, Card, Icon, CardTitle} from 'react-materialize';
 
 
 // anything that is materialize react specific has to be imported
 
 
-function NewsCard() {
+function NewsCard(props) {
   return (
 <Row>
   <Col
     m={6}
     s={12}
+    l={3}
   >
-    <CardPanel className="teal">
-      <span className="white-text">
-        For a simpler card with less markup, try using a card panel which just has padding and a shadow effect
-      </span>
-    </CardPanel>
+    <Card
+      actions={[
+        <a key={props.id} href={props.url}>{props.source}</a>
+      ]}
+      closeIcon={<Icon>close</Icon>}
+      header={<CardTitle image={props.urlToImage}>{props.title}</CardTitle>}
+      revealIcon={<Icon>more_vert</Icon>}
+    >
+      {props.description}
+    </Card>
   </Col>
 </Row>
   );
