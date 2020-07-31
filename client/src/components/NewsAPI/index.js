bcd1dd04411c711a0f8cc371f269dac05fce04bc
Armenuhi <avaarm95@gmail.com>
2020-07-30 20:57:55 -0700

    categories

--- client/src/components/NewsAPI/index.js
+++ client/src/components/NewsAPI/index.js
@@ -7,7 +7,8 @@ import { STATES } from "mongoose";
 function News() {
   const [news, setNews] = useState([]);
   const [newsOne, setNewsOne] = useState({});
-  // toggling between 5 vs all the results 
+
+  // toggling between 2 vs all the results 
   const [seeAll, toggleSeeAll] = useState(false)
 
   useEffect(() => {
@@ -41,34 +42,7 @@ function News() {
     <div>
       <Row>
 
-   
-
-        <Col m={6} s={12} l={3}>
-          <NewsCard
-            title={newsOne.title}
-            source={news[0] ? news[0].source.name : ""}
-            author={newsOne.author}
-            description={newsOne.description}
-            url={newsOne.url}
-            urlToImage={newsOne.urlToImage}
-            publishedAt={newsOne.publishedAt}
-            id="1"
-          />
-        </Col>
-        <Col m={6} s={12} l={3}>
-          <NewsCard
-            title="MANUAL LOADED INFO: Humans left behind a record amount of e-waste in 2019"
-            source="The Verge"
-            author="Justine Calma"
-            description="E-waste increased 21 percent over five years, according to a new global report. In 2019, people generated 53 million metric tons of electronic waste that poses a threat to the environment and human health."
-            url="https://www.theverge.com/21309776/record-amount-ewaste-2019-global-report-environment-health"
-            urlToImage="https://cdn.vox-cdn.com/thumbor/FyC7B8uFVNgBFa9YwmCkdLNKnvI=/0x336:5337x3130/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/20062074/1096277740.jpg.jpg"
-            publishedAt="2020-07-02T14:00:00Z"
-            id="1"
-          />
-        </Col>
         { !seeAll ?
-        
         news.slice(0,3).map((item, index) => (
             <Col m={6} s={12} l={3}>
                 <NewsCard
