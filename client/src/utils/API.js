054681f7b064c6a5c1e0ed687777ab0d3663a0de
Tyler McBride <tymcbrid@gmail.com>
2020-08-06 20:35:32 -0700

    merge

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,20 +2,19 @@ import axios from "axios";
 
 export default {
 
-////////NEWS API////////////////////
-getNews: function() {
-  const APIKEY_NEWS = process.env.REACT_APP_APIKEY_NEWS;
-  return axios.get("https://newsapi.org/v2/everything?q=environment&apiKey="+APIKEY_NEWS)
-},
+  ////////NEWS API////////////////////
+  getNews: function() {
+    const APIKEY_NEWS = process.env.REACT_APP_APIKEY_NEWS;
+    return axios.get("https://newsapi.org/v2/everything?q=environment&apiKey="+APIKEY_NEWS)
+  },
 
-//////////////coolClimate API//////////////////////
-// coolClimate: function() {
-//   const baseURL = process.env.REACT_APP_APIKEY_NEWS;
-//   const APIKEY = process.env.CoolClimate_APIKEY
-//   const ID = process.env.CoolClimate_ID
-//   console.log("")
-//   return axios.get("")
-// }
+  ////////////coolClimate API//////////////////////
+  getFootprint: function(inputType, input, income, householdSize) {
+    const APP_KEY = process.env.REACT_APP_COOLCLIMATE_APIKEY;
+    const APP_ID = process.env.REACT_APP_COOLCLIMATE_ID;
+    console.log(APP_KEY + " " + APP_ID);
+    return axios.get(`https://apis.berkeley.edu/coolclimate/footprint-defaults?app_id=${APP_ID}&app_key=${APP_KEY}&language=en&input_location_mode=${inputType}&input_location=${input}&input_income=${income}&input_size=${householdSize}`);
+  }
 
 };
 
