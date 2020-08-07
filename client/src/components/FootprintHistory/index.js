9e2162dae5b8e2d6c3a57a7cc136044baf8a64a4
Vinh Mao <vinhxmao87@gmail.com>
2020-08-06 20:29:55 -0700

    CoolClimate API calls initiated, XML parser code added

--- client/src/components/FootprintHistory/index.js
+++ client/src/components/FootprintHistory/index.js
@@ -1,26 +1,69 @@
-import React from "react";
-import { Row, Col, CardPanel} from 'react-materialize';
+import React, { useEffect, useState } from "react";
+import { Row, Col, CardPanel } from 'react-materialize';
 import "./style.css";
+import API from "../../utils/API";
 
+const parser = require("fast-xml-parser");
+const he = require("he");
 
-// anything that is materialize react specific has to be imported
+const options = {
+  attributeNamePrefix : "@_",
+  attrNodeName: "attr", //default is 'false'
+  textNodeName : "#text",
+  ignoreAttributes : true,
+  ignoreNameSpace : false,
+  allowBooleanAttributes : false,
+  parseNodeValue : true,
+  parseAttributeValue : false,
+  trimValues: true,
+  cdataTagName: "__cdata", //default is 'false'
+  cdataPositionChar: "\\c",
+  parseTrueNumberOnly: false,
+  arrayMode: false, //"strict"
+  attrValueProcessor: (val, attrName) => he.decode(val, {isAttributeValue: true}),//default is a=>a
+  tagValueProcessor : (val, tagName) => he.decode(val), //default is a=>a
+  stopNodes: ["parse-me-as-string"]
+};
 
+function FootprintHistory () {
 
-function FootprintHistory() {
-  return (
-<Row>
-  <Col
-    m={6}
-    s={12}
-  >
-    <CardPanel className="orangeCard">
-      <span className="white-text">
-        For a simpler card with less markup, try using a card panel which just has padding and a shadow effect
-      </span>
-    </CardPanel>
-  </Col>
-</Row>
-  );
-}
+  const [footprint, setFootprint] = useState([]);
+
+  useEffect(() => {
+    loadData();
+  }, []);  
+
+  function loadData() {
+    const inputType = "1";
+    const input = "98144";
+    const income = "5";
+    const householdSize = "4";
+    API.getFootprint(inputType, input, income, householdSize).then(res => {
+      const jsonData = parser.parse(res.data, options);
+      console.log(jsonData.response);
+      setFootprint(jsonData.response);
+    }
+  ).catch(err => console.log(err))};
+
+    return (
+      <Row>
+        <Col
+          l={6}
+          m={6}
+          s={12}
+        >
+          <CardPanel className="orangeCard">
+            <span className="white-text">
+              <p>You chose: {footprint.input_location}</p>
+              <p>The size of your household: {footprint.input_size}</p>
+              <p>Population of your zip code: {footprint.input_population}</p>
+              <p>Type of location chosen: {footprint.input_location_mode}</p>
+              <p>Income: {footprint.input_income}</p>
+            </span>
+          </CardPanel>
+        </Col>
+      </Row>
+    );
+  }
 
 export default FootprintHistory;
