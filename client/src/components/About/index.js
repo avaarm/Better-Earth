76f5a12f96caee5d0699ec2fdc551c3bf5b7ed0c
Tyler McBride <36458808+tymcbrid@users.noreply.github.com>
2020-08-11 14:31:15 -0700

    about page (#46)

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Row, Col, CardPanel } from "react-materialize";
+import earth from "./earth.jpg";
 import greta from "./greta.jpg";
 import "./style.css";
 
@@ -10,21 +11,47 @@ function About() {
     <Row>
       <Col m={14} s={20} l={25}>
         <CardPanel className="teal">
-          <div className="container center">
-            <h1 className="white-text center">Better Earth</h1>
-            <div className="white-text center">
+        <Row>
+          <Col className="zip" s={4} l={4}>
+          <h3 className="white-text center">Our Mission</h3>
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+        <Col s={3} l={3}></Col>
+        <Col s={7} l={7}>
+          <h6 className="white-text center">
+          Sustainable alternatives to everyday products exist, we’ll help you find the best ones
+            </h6>
+            {/* <div className="white-text center">
               Better Earth is an application that helps people find sustainable
               alternative to everyday products, such as clothing and household
               goods. We also provide the latest news on the environment as well
               as a carbon footprint functionality for our users to stay current
               on their envrionmental impact.
-            </div>
-            <hr />
-            <div className="container">
-            <img className="greta" alt="greta" src={greta} />
-            </div>
-           
-          </div>
+            </div> */}
+          </Col>
+        </Row>
+        <Row>
+        <Col className="center" s={10} l={10}>
+       <img className="greta" alt="earth" src={earth} />
+        </Col>
+        </Row>
+        <Row>
+        <Col className="quote" s={3} l={3}>
+        <p>"At first when I heard about climate change, I was a climate denier. I didn’t think it was happening. Because if there really was an existential crisis like that, that would threaten our civilisation, we wouldn’t be focusing on anything else." - Greta Thunberg</p>
+        </Col>
+        <Col className="center" s={9} l={9}>
+        <div className="container center">
+       
+       <div className="container">
+       <img className="greta" alt="greta" src={greta} />
+       </div>
+      
+     </div>
+        </Col>
+        </Row>
+
         </CardPanel>
       </Col>
     </Row>
