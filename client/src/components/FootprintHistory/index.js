888109823bf5f453f4e0b2f445487e358b41a209
Vinh Mao <vinhxmao87@gmail.com>
2020-08-11 20:07:51 -0700

    more data added to footprint card

--- client/src/components/FootprintHistory/index.js
+++ client/src/components/FootprintHistory/index.js
@@ -23,35 +23,40 @@ function FootprintHistory(props) {
               <thead>
                 <tr>
                   <th>Category</th>
-                  <th>Average consumption in household</th>
+                  <th>Average consumption in household/distance traveled</th>
                   <th>Possible action</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
                   <td>Electricity</td>
-                  <td>${result.input_footprint_housing_electricity_dollars}, or {Math.round(result.input_footprint_housing_electricity_kwh)} kWh per year</td>
+                  <td>${result.input_footprint_housing_electricity_dollars}/yr or {Math.round(result.input_footprint_housing_electricity_kwh)}kWh/yr</td>
                   <td>Turn down winter thermostat by {result.input_takeaction_thermostat_winter_daydegrees} degrees during the day and {result.input_takeaction_thermostat_winter_nightdegrees} at night</td>
                 </tr>
                 <tr>
-                  <td>Electricity</td>
-                  <td>${result.input_footprint_housing_electricity_dollars}, or {Math.round(result.input_footprint_housing_electricity_kwh)} kWh per year</td>
+                  <td>Transportation</td>
+                  <td>${result.input_footprint_transportation_publictrans}mi/yr on public transit,  {result.input_footprint_transportation_airtotal}mi/yr on air travel</td>
+                  <td>Carpool to work {result.input_takeaction_carpool_to_work_days} days per month, ride your bike for {result.input_takeaction_ride_my_bike_miles} miles per week, or telecommute to work {result.input_takeaction_telecommute_to_work_days} days per month</td>
+                </tr>
+                <tr>
+                  <td>Shopping</td>
+                  <td>${}</td>
                   <td>Turn down winter thermostat by {result.input_takeaction_thermostat_winter_daydegrees} degrees during the day and {result.input_takeaction_thermostat_winter_nightdegrees} at night</td>
                 </tr>
                 <tr>
-                  <td>Electricity</td>
-                  <td>${result.input_footprint_housing_electricity_dollars}, or {Math.round(result.input_footprint_housing_electricity_kwh)} kWh per year</td>
+                  <td>Waste</td>
+                  <td></td>
                   <td>Turn down winter thermostat by {result.input_takeaction_thermostat_winter_daydegrees} degrees during the day and {result.input_takeaction_thermostat_winter_nightdegrees} at night</td>
                 </tr>
                 <tr>
-                  <td>Electricity</td>
-                  <td>${result.input_footprint_housing_electricity_dollars}, or {Math.round(result.input_footprint_housing_electricity_kwh)} kWh per year</td>
-                  <td>Turn down winter thermostat by {result.input_takeaction_thermostat_winter_daydegrees} degrees during the day and {result.input_takeaction_thermostat_winter_nightdegrees} at night</td>
-                </tr>
-                <tr>
-                  <td>Electricity</td>
-                  <td>${result.input_footprint_housing_electricity_dollars}, or {Math.round(result.input_footprint_housing_electricity_kwh)} kWh per year</td>
-                  <td>Turn down winter thermostat by {result.input_takeaction_thermostat_winter_daydegrees} degrees during the day and {result.input_takeaction_thermostat_winter_nightdegrees} at night</td>
+                  <td>Food</td>
+                  <td>{result.result_takeaction_low_carbon_diet_cerealscal} calories for cereal, 
+                  {result.result_takeaction_low_carbon_diet_dairycal} calories for dairy,  
+                  {result.result_takeaction_low_carbon_diet_fvcal} calories for fruits and veggies, 
+                  {result.result_takeaction_low_carbon_diet_meatcal} calories for meat, 
+                  and {result.result_takeaction_low_carbon_diet_othercal} calories for all other food options, 
+                  for a total of {result.result_takeaction_low_carbon_diet_totalcal} calories per day</td>
+                  <td>For a low-carbon diet, eat less red meat and dairy than listed, and replace with chicken and fruits/veggies</td>
                 </tr>
               </tbody>
             </table>
