2e43d2b86b7815489d741d51893030f68d0125b7
jeffreyadamo <jeffreyadamo@gmail.com>
2020-08-14 11:53:30 -0700

    news can display more results; cleaned up extraneous api files

--- routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const db = require("../../models/");
-const productRoutes = require("./products");
-
-// Products routes
-router.use("/products", productRoutes);
-
-module.exports = router;
