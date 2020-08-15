ab7f956ce89f00e76fb1b0033b52aef2aa98441f
Armenuhi <avaarm95@gmail.com>
2020-08-14 23:56:40 -0700

    route debugging

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,7 +9,7 @@ const path = require("path");
 
 // If no API routes are hit, send the React app
 router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build','index.html'));
 });
 
 router.post("/login", (req, res, next) => {
